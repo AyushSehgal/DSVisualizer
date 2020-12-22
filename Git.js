24237657042bd0f5fdc662c12c050c9a1230fcf7
Ayush Sehgal <ayushs25@berkeley.edu>
2020-12-22 18:09:06 +0700

    clean up visual space

--- Git.js
+++ Git.js
@@ -1,17 +1,21 @@
 $('#terminal').terminal({
-    git: {
-        hello: () => {this.echo('Welcome')},
-        add: () => {git_add()},
-        commit: () => git_commit()
     
+    git: (command) => {
+        switch(command) {
+            case "add":
+                git_add();
+                break;
+            case "commit":
+                git_commit()
+                break;
+        }
+    }
     
-    
-    },
 }, {greetings: "Welcome to Terminal"});
 
 var fileID = 0
 function git_add() {
-    $('#staged').append($('<div class="col" style="float:left"><figure><figcaption style="font-size:12px">Img:' + fileID + '</figcaption><img src="./images/file.png" id="' + fileID + '" width="40" height="40"/></figure></div>'))
+    $('#staged').append($('<div class="col"><figure><figcaption style="font-size:12px">Img:' + fileID + '</figcaption><img src="./images/file.png" id="' + fileID + '" width="40" height="40"/></figure></div>'))
     fileID++
 }
 
