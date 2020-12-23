3c947cc077abe65e7ff12c924bc0cf4760ba0752
Ayush Sehgal <ayushs25@berkeley.edu>
2020-12-23 13:13:13 +0700

    add and commit bouncing logic

--- Git.js
+++ Git.js
@@ -1,9 +1,9 @@
-$('#terminal').terminal({
+var termObj = $('#terminal').terminal({
     
     git: (command) => {
         switch(command) {
             case "add":
-                git_add();
+                git_add()
                 break;
             case "commit":
                 git_commit()
@@ -11,15 +11,26 @@ $('#terminal').terminal({
         }
     }
     
-}, {greetings: "Welcome to Terminal"});
+}, {greetings: "Welcome to Terminal! \nTips:\n\tMake your own dummy files using the file keyword\n\tExample:\n\t\t\tfile [filename.extension]\n\t\t\tgit add [filename.extension] OR git add .\n\t\t\tgit commit -m \" Your message here\"\n\t\t\tgit push"});
 
-var fileID = 0
+var fileID = 0;
+var committed = false;
+var added = false;
 function git_add() {
     $('#staged').append($('<div class="col"><figure><figcaption style="font-size:12px">Img:' + fileID + '</figcaption><img src="./images/file.png" id="' + fileID + '" width="40" height="40"/></figure></div>'))
     fileID++
+    added = true;
 }
 
+
 function git_commit() {
-    return 0;
+    if (added && !committed) {
+        $('#commit').append($('<div class="col"><figure><figcaption style="font-size:12px">Img:' + added + '</figcaption><img src="./images/folder.png" width="40" height="40"/></figure></div>'))
+        committed = true;
+    } else if (committed) {
+        termObj.echo('Nothing to commit')
+    } else {
+        termObj.echo('Please add before committing ')
+    }
 }
 
