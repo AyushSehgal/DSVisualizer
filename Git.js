49e20ade190cab449814977d76b76102c9be1f3d
Ayush Sehgal <ayushs25@berkeley.edu>
2020-12-23 18:13:44 +0700

    add filename recognition for git add

--- Git.js
+++ Git.js
@@ -1,9 +1,12 @@
 var termObj = $('#terminal').terminal({
-    
-    git: (command) => {
+    pwd: () => {termObj.echo('./')},
+    ls: () => {ls()},
+    cd: () => {cd()},
+    touch: ()=> {touch()},
+    git: (command, args) => {
         switch(command) {
             case "add":
-                git_add()
+                git_add(args)
                 break;
             case "commit":
                 git_commit()
@@ -11,13 +14,29 @@ var termObj = $('#terminal').terminal({
         }
     }
     
-}, {greetings: "Welcome to Terminal! \nTips:\n\tMake your own dummy files using the file keyword\n\tExample:\n\t\t\tfile [filename.extension]\n\t\t\tgit add [filename.extension] OR git add .\n\t\t\tgit commit -m \" Your message here\"\n\t\t\tgit push"});
+}, {checkArity: false, greetings: "Welcome to Terminal! \nTips:\n\tMake your own dummy files using the file keyword\n\tExample:\n\t\t\tfile [filename.extension]\n\t\t\tgit add [filename.extension] OR git add .\n\t\t\tgit commit -m \" Your message here\"\n\t\t\tgit push"});
 
 var fileID = 0;
 var committed = false;
 var added = false;
-function git_add() {
-    $('#staged').append($('<div class="col"><figure><figcaption style="font-size:12px">Img:' + fileID + '</figcaption><img src="./images/file.png" id="' + fileID + '" width="40" height="40"/></figure></div>'))
+
+function cd() {
+    return 0
+}
+function ls() {
+    return 0
+}
+function touch() {
+    return 0
+}
+
+function git_add(args) {
+    if (args === undefined) {
+        termObj.echo('Please specify a file or multiple files to stage.')
+        return;
+    }
+
+    $('#staged').append($('<div class="col"><figure><figcaption style="font-size:12px">' + args + '</figcaption><img src="./images/file.png" id="' + fileID + '" width="40" height="40"/></figure></div>'))
     fileID++
     added = true;
 }
